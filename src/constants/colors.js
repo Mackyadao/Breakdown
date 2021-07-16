a412f25093fc6add319cd3c8261a77a4ad2e0c5b
LA Jambaro <eleyjambaro@gmail.com>
2021-07-16 18:31:52 +0800

    Change Login screen UI design

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,7 @@
 export const colors = {
     default: '#4072BD',
-    primary: '#cd3e21',
-    secondary: '#af7652',
+    primary: '#721003',
+    secondary: '#F8B966',
     tertiary: '#796054',
     quaternary: '#40322b',
     light: '#ffffff',
@@ -9,7 +9,7 @@ export const colors = {
     neutral: 'rgb(128, 128, 128)',
     neutralLighten1: 'rgb(153, 153, 153)',
     neutralLighten2: 'rgb(179, 179, 179)',
-    neutralLighten3: 'rgb(204, 204, 204)',
+    neutralLighten3: '#C4C4C4',
     neutralLighten4: '#DFDFDF',
     neutralLighten5: 'rgb(245, 245, 245)',
     danger: 'rgb(236, 31, 2)',
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,94 +1,126 @@
-import React from 'react';
-import {Text,View,Image, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Image, Pressable, StyleSheet} from 'react-native';
 
-export default class Login extends React.Component{
+import colors from '../constants/colors';
+import FormPillTextInput from '../components/forms/FormPillTextInput';
+import PrimaryPillFlatButton from '../buttons/PrimaryPillFlatButton';
 
-    render(){
-        const {navigate} = this.props.navigation
-        return(
-            <View style={{backgroundColor:"#FFF",height:"100%"}}>
-                <View style={{
-                    width:'100%',
-                    alignItems:"center",
-                    justifyContent:"center",
-                }}>
-                    <Image
-                    source={require('../images/logo.png')}
-                    style={{width:'100%'}}
-                    ></Image>
+const Login = props => {
+    const {navigation} = props;
+    const formFieldsInitState = {
+        mobileNumberUsernameOrEmail: '',
+        password: '',
+    };
+
+    const [formValues, setFormValues] = useState(formFieldsInitState);
+
+    const handleChangeText = (fieldValue, fieldName) => {
+        setFormValues(values => {
+            return {...values, [fieldName]: fieldValue};
+        });
+    };
+
+    const handleSubmit = () => {
+        navigation.navigate('Home');
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require('../assets/logo/lockup/lockup-vertical-colored-3x.png')}
+                />
+            </View>
+
+            <View style={styles.loginForm}>
+                <FormPillTextInput
+                    style={styles.pillTextInput}
+                    name="mobileNumberUsernameOrEmail"
+                    value={formValues.mobileNumberUsernameOrEmail}
+                    onChangeText={handleChangeText}
+                    placeholder="Mobile number, username, or email"
+                    autoCapitalize="none"
+                />
+
+                <FormPillTextInput
+                    style={[styles.pillTextInput, styles.pillTextInputSmall]}
+                    name="password"
+                    value={formValues.password}
+                    onChangeText={handleChangeText}
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    textContentType="password"
+                    secureTextEntry={true}
+                />
+
+                <View style={styles.forgotPasswordLinkContainer}>
+                    <Pressable>
+                        <Text style={styles.forgotPasswordLink}>
+                            Forgot Password?
+                        </Text>
+                    </Pressable>
                 </View>
 
-                <TextInput
-                    style={{
-                        borderWidth:1,
-                        borderTopLeftRadius:20,
-                        borderBottomLeftRadius:20,
-                        borderBottomRightRadius:20,
-                        borderTopRightRadius:20,
-                        marginHorizontal:'15%',
-                        marginTop:30,
-                        padding:10
-                    
-                    }}
-                    placeholder="Email or mobile number"
-                    keyboardType="default"
+                <PrimaryPillFlatButton
+                    style={[styles.button, styles.buttonSmall]}
+                    title="Login/Sign up"
+                    onPress={handleSubmit}
                 />
-
-                <TextInput
-                    style={{
-                        borderWidth:1,
-                        borderTopLeftRadius:20,
-                        borderBottomLeftRadius:20,
-                        borderBottomRightRadius:20,
-                        borderTopRightRadius:20,
-                        marginHorizontal:'15%',
-                        marginTop:10,
-                        padding:10
-                    
-                    }}
-                    placeholder="Password"
-                    keyboardType="default"
-                />
-                <Text    
-                            
-                style={{
-                    textAlign:'right',
-                    marginHorizontal:'13%',
-                    
-                }}>Forgot Password?</Text>
-                
-
-                <Text    
-                onPress={()=>navigate('Home')}      
-                style={{
-                    borderTopLeftRadius:20,
-                    borderBottomLeftRadius:20,
-                    borderBottomRightRadius:20,
-                    borderTopRightRadius:20,
-                    marginHorizontal:'15%',
-                    marginTop:15,
-                    backgroundColor:'#6C1717',
-                    padding:15,
-                    color:'#ffffff',
-                    textAlign:'center'
-                
-                }}>Login</Text>
-
-                <Text    
-                            
-                style={{
-                    textAlign:'center',
-                    marginTop:10,
-                    fontWeight:'bold'
-                }}>Don’t have an account? 
-                    <Text 
-                    onPress={()=>navigate('Register')}  
-                    style={{color: '#6C1717'
-                    }}> Sign up </Text> 
-                </Text>
-                
-            
             </View>
-        )
-    }
-}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.light,
+    },
+    logoContainer: {
+        width: '100%',
+        alignContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    logo: {
+        width: '100%',
+        height: 250,
+        resizeMode: 'contain',
+    },
+    loginForm: {
+        alignItems: 'center',
+        width: '100%',
+        padding: 40,
+    },
+    pillTextInput: {
+        marginVertical: 12,
+        width: '100%',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    pillTextInputSmall: {
+        width: '80%',
+    },
+    forgotPasswordLinkContainer: {
+        alignSelf: 'flex-end',
+        color: colors.dark,
+    },
+    forgotPasswordLink: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'right',
+    },
+    button: {
+        marginTop: 40,
+        marginBottom: 12,
+        width: '100%',
+    },
+    buttonSmall: {
+        width: '80%',
+    },
+});
+
+export default Login;
