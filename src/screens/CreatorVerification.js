89453c63bd46fe38716559072e3b7d93f07f1c1e
LA Jambaro <eleyjambaro@gmail.com>
2021-07-19 07:03:41 +0800

    Change the UI design of CreatorVerification screen

--- src/screens/CreatorVerification.js
+++ src/screens/CreatorVerification.js
@@ -1,120 +1,173 @@
-
 import React from 'react';
-import {Text,View,Image, TextInput, Icon, TouchableOpacity,ScrollView} from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    TextInput,
+    Icon,
+    TouchableOpacity,
+    ScrollView,
+} from 'react-native';
 
-export default class CreatorVerification extends React.Component{
+export default class CreatorVerification extends React.Component {
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <View style={{backgroundColor: '#ffffff', height: '100%'}}>
+                <Image
+                    source={require('../assets/logo/iconicMark/iconicMark-colored-3x.png')}
+                    style={{
+                        width: 42,
+                        height: 42,
+                        marginTop: 25,
+                        marginLeft: 25,
+                        borderColor: '#000',
+                        resizeMode: 'contain',
+                    }}
+                />
 
-
-    
-   
-    render(){
-        const {navigate} = this.props.navigation
-        return(
-            <View style={{backgroundColor:"#ffffff",height:"100%"}}>
-                
-                    <Image
-                    source={require('../images/logorec.png')}
-                    style={{width: 68,height:53,marginTop: 7, marginLeft: 8, position:'absolute'}}
+                <ScrollView
+                    style={{
+                        width: '100%',
+                        backgroundColor: '#FFFFFF',
+                        alignSelf: 'center',
+                        marginTop: 5,
+                        borderRadius: 30,
+                    }}>
+                    <Text
+                        style={{
+                            fontFamily: 'Oswald-Bold',
+                            fontSize: 35,
+                            alignSelf: 'center',
+                            color: '#333',
+                            textAlign: 'center',
+                        }}>
+                        Request Creator verification
+                    </Text>
+                    <Text
+                        style={{
+                            fontFamily: 'Montserrat-SemiBold',
+                            fontSize: 16,
+                            padding: 20,
+                            alignSelf: 'center',
+                            color: '#000',
+                            textAlign: 'justify',
+                            marginTop: 20,
+                        }}>
+                        A Verified Creator account is represented by a golden
+                        “V”, placed next to the username. This badge indicates
+                        the account is the authentic presence of a creator,
+                        notable public figure, celebrity of the arts, global
+                        brand or entity it represents.
+                        {'\n'}
+                        {'\n'}Submitting a request will guarantee consideration,
+                        but will not guarantee verification.
+                    </Text>
+                    <TextInput
+                        style={{
+                            width: '100%',
+                            borderTopWidth: 2,
+                            borderBottomWidth: 1,
+                            marginTop: 10,
+                            padding: 10,
+                        }}
+                        placeholder="Username"
+                        keyboardType="default"
                     />
-
-                    
-                   
-                    <ScrollView style={{width:'100%',  backgroundColor:'#FFFFFF', alignSelf:'center',marginTop:70,borderTopLeftRadius:30, borderTopRightRadius:30, borderBottomLeftRadius:30, borderBottomRightRadius:30}}>
-                        <Text style={{fontFamily:"Pattaya-Regular",fontSize:35, alignSelf:'center', color:"#333", textAlign:'center'}}>Request Creator verification</Text>
-                        <Text style={{fontFamily:"Montserrat-SemiBold",fontSize:16, padding:20, alignSelf:'center', color:"#000", textAlign:'justify',marginTop:20}}>
-                            A Verified Creator account is represented by a golden “V”, 
-                            placed next to the username. This badge indicates the account is the authentic presence of a creator, 
-                            notable public figure, celebrity of the arts, global brand or entity it represents.
-                            {"\n"}{"\n"}Submitting a request will guarantee consideration, but will not guarantee verification.
-
-
+                    <TextInput
+                        style={{
+                            width: '100%',
+                            borderBottomWidth: 1,
+                            padding: 10,
+                        }}
+                        placeholder="Fullname"
+                        keyboardType="default"
+                    />
+                    <TextInput
+                        style={{
+                            width: '100%',
+                            borderBottomWidth: 1,
+                            padding: 10,
+                        }}
+                        placeholder="Known As"
+                        keyboardType="default"
+                    />
+                    <TextInput
+                        style={{
+                            width: '100%',
+                            borderBottomWidth: 2,
+                            padding: 10,
+                        }}
+                        placeholder="Art Category"
+                        keyboardType="default"
+                    />
+                    <Text
+                        style={{
+                            fontFamily: 'Montserrat-SemiBold',
+                            fontSize: 16,
+                            padding: 20,
+                            alignSelf: 'center',
+                            color: '#000',
+                            textAlign: 'justify',
+                            marginTop: 20,
+                        }}>
+                        Please attach a copy of your photo ID (feel free to
+                        block out residential information), submit proof of
+                        work, and a 5 second video stating your name, today’s
+                        date, and that you are joining The Breakdown App.
+                    </Text>
+                    <Text
+                        style={{
+                            fontFamily: 'Montserrat-Medium',
+                            fontSize: 16,
+                            padding: 20,
+                            alignSelf: 'center',
+                            color: '#000',
+                            textAlign: 'justify',
+                        }}>
+                        <Text
+                            style={{
+                                color: '#4072BD',
+                                fontFamily: 'Montserrat-SemiBold',
+                            }}>
+                            Choose file
                         </Text>
-                        <TextInput
+                        {'\n'}
+                        {'\n'}
+                        {'\n'}
+                        We require a government-issued photo ID displaying your
+                        name and date of birth (e.g. driver’s license, passport
+                        or national identification card). Official business
+                        documents (tax filing, recent utility bill, article of
+                        incorporation) are also acceptable forms of
+                        identification needed to view your request.
+                    </Text>
+                    <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                        <Text
+                            onPress={() => navigate('Home')}
                             style={{
-                                width:'100%',
-                                borderTopWidth:2,
-                                borderBottomWidth:1,
-                                marginTop:10,
-                                padding:10
-                            
-                            }}
-                            placeholder="Username"
-                            keyboardType="default"
-                        />
-                        <TextInput
-                            style={{
-                                width:'100%',
-                                borderBottomWidth:1,
-                                padding:10
-                            
-                            }}
-                            placeholder="Fullname"
-                            keyboardType="default"
-                        />
-                        <TextInput
-                            style={{
-                                width:'100%',
-                                borderBottomWidth:1,
-                                padding:10
-                            
-                            }}
-                            placeholder="Known As"
-                            keyboardType="default"
-                        />
-                        <TextInput
-                            style={{
-                                width:'100%',
-                                borderBottomWidth:2,
-                                padding:10
-                            
-                            }}
-                            placeholder="Art Category"
-                            keyboardType="default"
-                        />
-                        <Text style={{fontFamily:"Montserrat-SemiBold",fontSize:16, padding:20, alignSelf:'center', color:"#000", textAlign:'justify',marginTop:20}}>
-                        Please attach a copy of your photo ID (feel free to block out residential information), 
-                        submit proof of work, and a 5 second video stating your name,
-                        today’s date, and that you are joining The Breakdown App.   
-
-                             </Text>
-                             <Text style={{fontFamily:"Montserrat-Medium",fontSize:16, padding:20, alignSelf:'center', color:"#000", textAlign:'justify'}}>
-                            <Text style={{color:'#4072BD',fontFamily:"Montserrat-SemiBold"}}>Choose file</Text>{"\n"}{"\n"}{"\n"}
-
-                            We require a government-issued photo ID displaying your name and date of birth (e.g. driver’s license, passport or national identification card). 
-                            Official business documents (tax filing, recent utility bill, 
-                            article of incorporation) are also acceptable forms of 
-                            identification needed to view your request.
-
-
-
-                             </Text>
-                        <View style={{flexDirection:'row', flexWrap:'wrap',}}>
-                            <Text    
-                            onPress={()=>navigate('Home')}      
-                            style={{
-                                borderTopLeftRadius:30,
-                                borderBottomLeftRadius:30,
-                                borderBottomRightRadius:30,
-                                borderTopRightRadius:30,
-                                borderWidth:2,
-                                borderColor:'#aaa',
-                                marginTop:15,
-                                flex:1,
-                                backgroundColor:'#4072BD',
-                                padding:15,
-                                fontSize:16,
-                                marginHorizontal:50,
-                                marginBottom:50,
-                                color:'#FFFFFF',
-                                textAlign:'center',
-                                fontFamily:'Montserrat-SemiBold',
-                            }}>Send</Text>
-                            
-                            
-                        </View>
-                    </ScrollView>
-                
+                                borderTopLeftRadius: 30,
+                                borderBottomLeftRadius: 30,
+                                borderBottomRightRadius: 30,
+                                borderTopRightRadius: 30,
+                                borderWidth: 2,
+                                borderColor: '#aaa',
+                                marginTop: 15,
+                                flex: 1,
+                                backgroundColor: '#4072BD',
+                                padding: 15,
+                                fontSize: 16,
+                                marginHorizontal: 50,
+                                marginBottom: 50,
+                                color: '#FFFFFF',
+                                textAlign: 'center',
+                                fontFamily: 'Montserrat-SemiBold',
+                            }}>
+                            Send
+                        </Text>
+                    </View>
+                </ScrollView>
             </View>
-        )
+        );
     }
 }
--- src/screens/RTidentification1.js
+++ src/screens/RTidentification1.js
@@ -1,72 +1,121 @@
+import {co} from 'co';
 import React from 'react';
-import {Text,View,Image, TextInput, Icon, TouchableOpacity} from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    TextInput,
+    Icon,
+    TouchableOpacity,
+} from 'react-native';
 
-export default class RTidentification1 extends React.Component{
+import colors from '../constants/colors';
 
+export default class RTidentification1 extends React.Component {
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <View style={{backgroundColor: colors.light, height: '100%'}}>
+                <Image
+                    source={require('../assets/logo/iconicMark/iconicMark-colored-3x.png')}
+                    style={{
+                        width: 42,
+                        height: 42,
+                        marginTop: 25,
+                        marginLeft: 25,
+                        borderColor: '#000',
+                        resizeMode: 'contain',
+                    }}
+                />
 
-    
-   
-    render(){
-        const {navigate} = this.props.navigation
-        return(
-            <View style={{backgroundColor:"#721003",height:"100%"}}>
-                
-                    <Image
-                    source={require('../images/logorec.png')}
-                    style={{width: 68,height:53,marginTop: 7, marginLeft: 8, position:'absolute'}}
-                    />
-
-                    <View style={{width:'80%',  backgroundColor:'#FAC180', alignSelf:'center', marginTop: 100, padding:20,borderTopLeftRadius:30, borderTopRightRadius:30, borderBottomLeftRadius:30, borderBottomRightRadius:30}}>
-                        <Text style={{fontFamily:"Pattaya-Regular",fontSize:35, alignSelf:'center', color:"#333", textAlign:'center'}}>Are you a Creator{"\n"}of arts?</Text>
-                        <View style={{flexDirection:'row', flexWrap:'wrap',}}>
-                            <Text    
-                            onPress={()=>navigate('RTidentification2')}      
+                <View
+                    style={{
+                        width: '80%',
+                        backgroundColor: colors.neutralLighten3,
+                        alignSelf: 'center',
+                        marginTop: 100,
+                        padding: 20,
+                        borderTopLeftRadius: 30,
+                        borderTopRightRadius: 30,
+                        borderBottomLeftRadius: 30,
+                        borderBottomRightRadius: 30,
+                    }}>
+                    <Text
+                        style={{
+                            fontFamily: 'Oswald-Bold',
+                            fontSize: 35,
+                            alignSelf: 'center',
+                            color: colors.dark,
+                            textAlign: 'center',
+                        }}>
+                        Are you a Creator{'\n'}of the arts?
+                    </Text>
+                    <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                        <Text
+                            onPress={() => navigate('MakeItOfficial')}
                             style={{
-                                borderTopLeftRadius:30,
-                                borderBottomLeftRadius:30,
-                                borderBottomRightRadius:30,
-                                borderTopRightRadius:30,
-                                borderWidth:2,
-                                borderColor:'#aaa',
-                                marginTop:15,
-                                flex:1,
-                                backgroundColor:'#FFFFFF',
-                                padding:15,
-                                fontSize:18,
-                                marginHorizontal:5,
-                                color:'#6C1717',
-                                textAlign:'center',
-                                fontFamily:'Montserrat-SemiBold',
-                            }}>Yes</Text>
-                            <Text    
-                            onPress={()=>navigate('Home')}      
+                                borderTopLeftRadius: 30,
+                                borderBottomLeftRadius: 30,
+                                borderBottomRightRadius: 30,
+                                borderTopRightRadius: 30,
+                                borderWidth: 2,
+                                borderColor: '#aaa',
+                                marginTop: 15,
+                                flex: 1,
+                                backgroundColor: '#FFFFFF',
+                                padding: 15,
+                                fontSize: 18,
+                                marginHorizontal: 5,
+                                color: '#6C1717',
+                                textAlign: 'center',
+                                fontFamily: 'Montserrat-SemiBold',
+                            }}>
+                            Yes
+                        </Text>
+                        <Text
+                            onPress={() => navigate('Home')}
                             style={{
-                                borderTopLeftRadius:30,
-                                borderBottomLeftRadius:30,
-                                borderBottomRightRadius:30,
-                                borderTopRightRadius:30,
-                                borderWidth:2,
-                                borderColor:'#aaa',
-                                marginTop:15,
-                                flex:1,
-                                backgroundColor:'#FFFFFF',
-                                padding:15,
-                                fontSize:18,
-                                marginHorizontal:5,
-                                color:'#6C1717',
-                                textAlign:'center',
-                                fontFamily:'Montserrat-SemiBold',
-                            
-                            }}>No</Text>
-                        </View>
+                                borderTopLeftRadius: 30,
+                                borderBottomLeftRadius: 30,
+                                borderBottomRightRadius: 30,
+                                borderTopRightRadius: 30,
+                                borderWidth: 2,
+                                borderColor: '#aaa',
+                                marginTop: 15,
+                                flex: 1,
+                                backgroundColor: '#FFFFFF',
+                                padding: 15,
+                                fontSize: 18,
+                                marginHorizontal: 5,
+                                color: '#6C1717',
+                                textAlign: 'center',
+                                fontFamily: 'Montserrat-SemiBold',
+                            }}>
+                            No
+                        </Text>
                     </View>
-
-                    <View style={{width:'80%',  backgroundColor:'#FFFFFF',borderWidth:2, borderColor:'#555', alignSelf:'center', marginTop: 50, padding:20,borderTopLeftRadius:30, borderTopRightRadius:30, borderBottomLeftRadius:30, borderBottomRightRadius:30}}>
-                        <Text style={{fontFamily:'Montserrat-Medium', fontSize:18,}}>Are you a Creator (aligned to the Film, TV, or Book industries) looking to build a stronger connection with your audience?  This title is designated for you!</Text>
-                    </View>
-                    
-                
+                </View>
+                <View
+                    style={{
+                        width: '80%',
+                        backgroundColor: '#FFFFFF',
+                        borderWidth: 2,
+                        borderColor: '#555',
+                        alignSelf: 'center',
+                        marginTop: 50,
+                        padding: 20,
+                        borderTopLeftRadius: 30,
+                        borderTopRightRadius: 30,
+                        borderBottomLeftRadius: 30,
+                        borderBottomRightRadius: 30,
+                    }}>
+                    <Text style={{fontFamily: 'Roboto-Regular', fontSize: 18}}>
+                        Are you a Creator (aligned to the Film, TV, or Book
+                        industries) looking to build a stronger connection with
+                        your audience? This title is designated for you!
+                    </Text>
+                </View>
             </View>
-        )
+        );
     }
 }
