bc44855b9d01171c6532c264566105aeec52c677
LA Jambaro <eleyjambaro@gmail.com>
2021-09-19 19:11:02 +0800

    Add Tier selection section to CreatorVerification screen

--- src/screens/CreatorVerification.js
+++ src/screens/CreatorVerification.js
@@ -1,59 +1,151 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Text,
     View,
     Image,
     TextInput,
+    Pressable,
     ScrollView,
     StyleSheet,
 } from 'react-native';
 
 import colors from '../constants/colors';
+import RadioButton from '../components/buttons/RadioButton';
 import DefaultPillFlatButton from '../components/buttons/DefaultPillFlatButton';
 
-export default class CreatorVerification extends React.Component {
-    render() {
-        const {navigate} = this.props.navigation;
-        return (
-            <View style={{backgroundColor: '#ffffff', height: '100%'}}>
-                <Image
-                    source={require('../assets/logo/iconicMark/iconicMark-colored-3x.png')}
-                    style={{
-                        width: 42,
-                        height: 42,
-                        marginTop: 25,
-                        marginLeft: 25,
-                        borderColor: '#000',
-                        resizeMode: 'contain',
-                    }}
-                />
+const TierOne = props => {
+    return (
+        <View style={styles.tierOptionItemValueContainer}>
+            <Pressable {...props} style={styles.tierOptionItemValueHeader}>
+                <Text style={[styles.contentText, styles.boldText]}>
+                    Tier 1
+                </Text>
+            </Pressable>
 
-                <ScrollView
-                    style={{
-                        width: '100%',
-                        backgroundColor: '#FFFFFF',
-                        alignSelf: 'center',
-                        marginTop: 5,
-                        borderRadius: 30,
-                    }}>
-                    <Text
-                        style={{
-                            fontFamily: 'Oswald-Bold',
-                            fontSize: 35,
-                            alignSelf: 'center',
-                            color: colors.dark,
-                            textAlign: 'center',
-                        }}>
-                        Request Creator verification
+            <View style={styles.tierOptionItemValueDescContainer}>
+                <View style={styles.tierRequirementsList}>
+                    <View style={styles.tierRequirementsListItem}>
+                        <Text style={styles.tierRequirementsListItemBullet}>
+                            •
+                        </Text>
+                        <Text style={[styles.contentText]}>
+                            Submit proof of work
+                        </Text>
+                        <Pressable style={styles.infoIconContainer}>
+                            <Image
+                                source={require('../assets/icons/info-solid-3x.png')}
+                                style={styles.infoIcon}
+                            />
+                        </Pressable>
+                    </View>
+
+                    <View style={styles.tierRequirementsListItem}>
+                        <Text style={styles.tierRequirementsListItemBullet}>
+                            •
+                        </Text>
+                        <Text style={[styles.contentText]}>
+                            Upload a copy of photo ID (feel free to block out
+                            residential information)
+                        </Text>
+                    </View>
+
+                    <View style={styles.tierRequirementsListItem}>
+                        <Text style={styles.tierRequirementsListItemBullet}>
+                            •
+                        </Text>
+                        <Text style={[styles.contentText]}>
+                            Upload a 5 second video stating your name, today’s
+                            date, confirm that you’re joining The Breakdown App
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const TierTwo = props => {
+    return (
+        <View style={styles.tierOptionItemValueContainer}>
+            <Pressable {...props} style={styles.tierOptionItemValueHeader}>
+                <Text style={[styles.contentText, styles.boldText]}>
+                    Tier 2
+                </Text>
+            </Pressable>
+
+            <View style={styles.tierOptionItemValueDescContainer}>
+                <View style={styles.tierRequirementsList}>
+                    <View style={styles.tierRequirementsListItem}>
+                        <Text style={styles.tierRequirementsListItemBullet}>
+                            •
+                        </Text>
+                        <Text style={[styles.contentText]}>
+                            Submit proof of work
+                        </Text>
+                        <Pressable style={styles.infoIconContainer}>
+                            <Image
+                                source={require('../assets/icons/info-solid-3x.png')}
+                                style={styles.infoIcon}
+                            />
+                        </Pressable>
+                    </View>
+
+                    <View style={styles.tierRequirementsListItem}>
+                        <Text style={styles.tierRequirementsListItemBullet}>
+                            •
+                        </Text>
+                        <Text style={[styles.contentText]}>
+                            Upload a 5 second video stating name, today’s date,
+                            confirm that you’re joining The Breakdown App
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const tierOptions = [
+    {
+        key: 'Tier 1',
+        renderValue: TierOne,
+    },
+    {
+        key: 'Tier 2',
+        renderValue: TierTwo,
+    },
+];
+
+const CreatorVerification = props => {
+    const {navigation} = props;
+
+    return (
+        <View style={styles.screen}>
+            <ScrollView style={styles.contentContainer}>
+                <View>
+                    <Image
+                        source={require('../assets/logo/iconicMark/iconicMark-colored-3x.png')}
+                        style={styles.icon}
+                    />
+                </View>
+
+                <View style={styles.header}>
+                    <Pressable
+                        onPress={() => navigation.goBack()}
+                        style={styles.goBackArrowContainer}>
+                        <Image
+                            source={require('../assets/icons/go-back-arrow-3x.png')}
+                            style={styles.goBackArrow}
+                        />
+                    </Pressable>
+
+                    <Text style={styles.heading}>
+                        Request Creator Verification
                     </Text>
-                    <Text
-                        style={{
-                            fontFamily: 'Roboto',
-                            fontSize: 16,
-                            padding: 20,
-                            alignSelf: 'center',
-                            color: '#000',
-                        }}>
+                </View>
+
+                <View style={styles.contentWrapper}>
+                    <Text style={styles.contentText}>
                         <Text style={styles.boldText}>Tier 1</Text> Verified
                         Creators are represented by a red ring around their
                         avatar, and indicates that they are a Creator affiliated
@@ -61,10 +153,12 @@ export default class CreatorVerification extends React.Component {
                         {'\n\n'}
                         <Text style={styles.boldText}>Tier 2</Text> Verified
                         Creator avatars will wear the red ring, and also be
-                        represented by a golden “V“ alongside their usernames.
-                        This status is reserved for those with sizeable social
-                        media followings, or notable public figures or
-                        celebrities in the arts & entertainment communities.
+                        represented by a golden "
+                        <Text style={styles.verifiedCreatorBadge}>V</Text>"
+                        alongside their usernames. This status is reserved for
+                        those with sizeable social media followings, or notable
+                        public figures or celebrities in the arts &
+                        entertainment communities.
                         {'\n\n'}
                         This process ensures all Creator accounts are the
                         authentic presence of a Creator, notable public figure,
@@ -73,97 +167,250 @@ export default class CreatorVerification extends React.Component {
                         will also be unlocked.
                         {'\n\n'}
                         Submitting a request will guarantee consideration, but
-                        will not guarantee verification.
+                        will not guarantee verification
                     </Text>
-                    <TextInput
-                        style={{
-                            width: '100%',
-                            borderTopWidth: 2,
-                            borderBottomWidth: 1,
-                            padding: 10,
-                        }}
-                        placeholder="Username"
-                        keyboardType="default"
-                    />
-                    <TextInput
-                        style={{
-                            width: '100%',
-                            borderBottomWidth: 1,
-                            padding: 10,
-                        }}
-                        placeholder="Fullname"
-                        keyboardType="default"
-                    />
-                    <TextInput
-                        style={{
-                            width: '100%',
-                            borderBottomWidth: 2,
-                            padding: 10,
-                        }}
-                        placeholder="Known As"
-                        keyboardType="default"
-                    />
+                </View>
 
-                    <View
-                        style={{
-                            padding: 20,
-                            marginTop: 30,
+                <TextInput
+                    style={[
+                        styles.textInput,
+                        {
                             borderTopWidth: 2,
+                        },
+                    ]}
+                    placeholder="Username"
+                    placeholderTextColor={colors.dark}
+                    keyboardType="default"
+                />
+
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Fullname"
+                    placeholderTextColor={colors.dark}
+                    keyboardType="default"
+                />
+
+                <TextInput
+                    style={[styles.textInput]}
+                    placeholder="Known As"
+                    placeholderTextColor={colors.dark}
+                    keyboardType="default"
+                />
+
+                <View
+                    style={[
+                        styles.dropDownToggleContainer,
+                        {
                             borderBottomWidth: 2,
-                        }}>
+                        },
+                    ]}>
+                    <Text style={styles.dropDownToggleText}>
+                        {'Arts & Entertainment Category'}
+                    </Text>
+
+                    <Text style={[styles.dropDownLabelText, styles.boldText]}>
+                        Please make your selection from the drop down box{'  '}
+                        <Image
+                            style={styles.dropDownLabelArrow}
+                            source={require('../assets/icons/drop-down-arrow-solid-3x.png')}
+                        />
+                    </Text>
+                </View>
+
+                <View style={styles.tierSelectionSection}>
+                    <View style={styles.contentWrapper}>
                         <Text
-                            style={{
-                                fontSize: 16,
-                                color: colors.dark,
-                            }}>
-                            <Text
-                                style={{
-                                    color: '#4072BD',
-                                    fontWeight: 'bold',
-                                }}>
-                                Choose file(s)
-                            </Text>
-                            {'\n\n'}
-                            For tier 2 we require a government-issued photo ID
-                            displaying your name and date of birth (e.g.
-                            driver’s license, passport or national
-                            identification card). Official business documents
-                            (tax filing, recent utility bill, article of
-                            incorporation) are also acceptable forms of
-                            identification needed to view your request.
-                            {'\n\n'}
+                            style={[
+                                styles.contentText,
+                                styles.boldText,
+                                styles.tierSelectionHeading,
+                            ]}>
+                            Requirements | Make your tier selection below
                         </Text>
 
-                        <Text
-                            style={{
-                                fontSize: 16,
-                                color: colors.dark,
-                                textAlign: 'justify',
-                            }}>
-                            Enter proof of work here or upload a files above.
-                        </Text>
-                    </View>
-
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            flexWrap: 'wrap',
-                            justifyContent: 'space-evenly',
-                            marginVertical: 20,
-                        }}>
-                        <DefaultPillFlatButton
-                            title="Send"
-                            onPress={() => navigate('Home')}
+                        <RadioButton
+                            options={tierOptions}
+                            optionItemContainerStyle={
+                                styles.tierOptionItemContainer
+                            }
+                            radioCircleStyle={{marginTop: 2}}
                         />
                     </View>
-                </ScrollView>
-            </View>
-        );
-    }
-}
+                </View>
+
+                <View
+                    style={{
+                        padding: 20,
+                        borderTopWidth: 1,
+                        borderBottomWidth: 1,
+                    }}>
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            color: colors.dark,
+                        }}>
+                        <Text
+                            style={{
+                                color: '#4072BD',
+                                fontWeight: 'bold',
+                            }}>
+                            Choose file(s)
+                        </Text>
+                        {'\n\n'}
+                        For tier 2 we require a government-issued photo ID
+                        displaying your name and date of birth (e.g. driver’s
+                        license, passport or national identification card).
+                        Official business documents (tax filing, recent utility
+                        bill, article of incorporation) are also acceptable
+                        forms of identification needed to view your request.
+                        {'\n\n'}
+                    </Text>
+
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            color: colors.dark,
+                            textAlign: 'justify',
+                        }}>
+                        Enter proof of work here or upload a files above.
+                    </Text>
+                </View>
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-evenly',
+                        marginVertical: 20,
+                    }}>
+                    <DefaultPillFlatButton
+                        title="Send"
+                        onPress={() => navigation.navigate('Home')}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
+    screen: {
+        backgroundColor: colors.light,
+        height: '100%',
+    },
+    icon: {
+        width: 42,
+        height: 42,
+        marginTop: 25,
+        marginLeft: 25,
+        resizeMode: 'contain',
+    },
+    goBackArrowContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 28,
+    },
+    goBackArrow: {
+        width: 34,
+        height: 34,
+    },
+    contentContainer: {
+        width: '100%',
+        backgroundColor: colors.light,
+        alignSelf: 'center',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    heading: {
+        marginHorizontal: 50,
+        fontFamily: 'Oswald-Bold',
+        fontSize: 35,
+        alignSelf: 'center',
+        color: colors.dark,
+        textAlign: 'center',
+    },
+    contentWrapper: {
+        margin: 18,
+    },
+    contentText: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        color: colors.dark,
+    },
     boldText: {
         fontWeight: '700',
+        color: colors.dark,
+    },
+    textInput: {
+        width: '100%',
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        paddingHorizontal: 28,
+        fontSize: 16,
+    },
+    dropDownToggleContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 28,
+        borderBottomWidth: 1,
+    },
+    dropDownToggleText: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+    },
+    dropDownLabelText: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+    },
+    dropDownLabelArrow: {
+        height: 14,
+        width: 16,
+        resizeMode: 'contain',
+    },
+    verifiedCreatorBadge: {
+        height: 39,
+        width: 8,
+        fontFamily: 'Oswald',
+        fontWeight: 'bold',
+        color: '#D88912',
+    },
+    infoIconContainer: {
+        paddingVertical: 2,
+        paddingHorizontal: 10,
+        alignSelf: 'center',
+    },
+    infoIcon: {
+        width: 16,
+        height: 16,
+    },
+    tierSelectionSection: {},
+    tierSelectionHeading: {
+        marginBottom: 7,
+    },
+    tierOptionItemContainer: {
+        marginBottom: 7,
+        alignItems: 'flex-start',
+    },
+    tierOptionItemValueContainer: {
+        flex: 1,
+    },
+    tierOptionItemValueHeader: {
+        marginRight: 'auto',
+        paddingLeft: 7,
+        paddingRight: 80,
+    },
+    tierOptionItemValueDescContainer: {
+        marginLeft: -14,
+    },
+    tierRequirementsList: {},
+    tierRequirementsListItem: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    tierRequirementsListItemBullet: {
+        marginRight: 8,
     },
 });
+
+export default CreatorVerification;
